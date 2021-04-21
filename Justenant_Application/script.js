d4b108759ce3a172b1f61dc74007a7dbfb3a2d2a
henrikauzins <henrikauzins@hotmail.com>
2021-04-21 13:45:33 +0100

    updated legalaid.html page to client brief specifications for tenant report generation

--- Justenant_Application/script.js
+++ Justenant_Application/script.js
@@ -3,75 +3,56 @@ function createPDF() {
 	var doc = new jsPDF();
 
 	doc.text("Tenant Report", 90, 10);
-	doc.text("Housing Problem: " + document.getElementById("housing-problem").value, 20, 20);
-	var ai = document.getElementById("annual-income").value;
+
+	var ai = document.getElementById("rent").value;
 
 	if (ai != "")
-		doc.text("Annual Income (£): " + ai, 20, 30);
+		doc.text("Rent owed (£): " + ai, 20, 20);
 	else {
-		alert("ERROR..!!..Please Enter Income. ");
+		alert("ERROR..!!..Please Enter Rent. ");
 		return;
 	}
-
 
 	ai = document.getElementById("current-residence").value;
 
 	if (ai != "")
-		doc.text("Time at Current Residence (weeks): " + document.getElementById("current-residence").value, 20, 40);
+		doc.text("Time at Current Residence (weeks): " + document.getElementById("current-residence").value, 20, 30);
 	else {
 		alert("ERROR..!!..Please Enter Current Residence. ");
 		return;
 	}
-
-	if (document.getElementById("yes").checked || document.getElementById("no").checked)
-		document.getElementById("yes").checked == true ? doc.text("Partner: Yes", 20, 50) : doc.text("Partner: No", 20, 50);
+	ai = document.getElementById("occupant").value;
+	if (ai != "")
+		doc.text("Occupants: " + ai, 20, 40);
 	else {
-		alert("ERROR..!!..Please Check the Partner. ");
+		alert("ERROR..!!..Please Enter number of occupants. ");
 		return;
 	}
 
-	if (document.getElementById("employment").value != "")
-		doc.text("Employment Status: " + document.getElementById("employment").value, 20, 60);
+	ai = document.getElementById("annual-income").value;
+	if (ai != "")
+		doc.text("Annual Income (£): " + ai, 20, 50);
 	else {
-		alert("ERROR..!!..Please Enter Employement Value. ");
+		alert("ERROR..!!..Please Enter your Annual Income. ");
+		return;
+	}
+
+
+	if (document.getElementById("yes").checked || document.getElementById("no").checked)
+		document.getElementById("yes").checked == true ? doc.text("Significant mental/physical problems: Yes", 20, 60) : doc.text("Significant mental/physical problems: No", 20, 60);
+	else {
+		alert("ERROR..!!..Please Check health problems. ");
 		return;
 	}
 
 	if (document.getElementById("yes1").checked || document.getElementById("no1").checked) {
-		document.getElementById("yes1").checked == true ? doc.text("Savings/Investments: Yes", 20, 70) : doc.text("Savings/Investments: No", 20, 70);
+		document.getElementById("yes1").checked == true ? doc.text("Disrepair to home: Yes", 20, 70) : doc.text("Disrepair to home: No", 20, 70);
 	}
 	else {
-		alert("ERROR..!!..Please Check the Savings/Investments. ");
+		alert("ERROR..!!..Please Check Disrepair to Home. ");
 		return;
 	}
-
-
-	if (document.getElementById("children").value != "")
-		doc.text("Number of Children: " + document.getElementById("children").value, 20, 80);
-	else {
-		alert("ERROR..!!..Please Enter No of Children. ");
-		return;
-	}
-
-	if (document.getElementById("dependents").value != "")
-		doc.text("Number of Dependents: " + document.getElementById("dependents").value, 20, 90);
-	else {
-		alert("ERROR..!!..Please Enter No of dependents. ");
-		return;
-	}
-
-
-	if (document.getElementById("yes2").checked || document.getElementById("no2").checked) {
-		document.getElementById("yes2").checked == true ? doc.text("Benefits: Yes", 20, 100) : doc.text("Benefits: No", 20, 100);
-	}
-	else {
-		alert("ERROR..!!..Please Check the Benefits. ");
-		return;
-	}
-
-
 
 	doc.save("tenant-report.pdf");
-	alert("Congratulations. Your form is submitted, Please explore the document in pdf format. ");
+	alert("Congrats. Your form is submitted, Please explore the document in pdf format. ");
 }
-

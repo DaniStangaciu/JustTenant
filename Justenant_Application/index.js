5acfca3206277b878db2d276e1d070311f7729e5
Daniel Stangaciu <48733629+DaniStangaciu@users.noreply.github.com>
2021-05-12 22:19:45 +0300

    Add files via upload

--- Justenant_Application/index.js
+++ Justenant_Application/index.js
@@ -1,18 +1,48 @@
 ﻿// Initialize and add the map
 function initMap() {
-    // Location = London
-    const london = {
-        lat: 51.5018,
-        lng: 0.0910
-    };
-    // The map, centered in London
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 10,
-        center: london,
+    var locations = [
+
+        ['Anthony Gold', 51.50537, -0.08443, 2],
+        ['Deighton Pierce Glynn', 51.5311, 0.1037, 5],
+        ['Gans & Co Solicitors LLP', 51.4665, 0.0662, 6],
+        ['GT Stewart', 51.460935606148134, -0.08014574400738096, 6],
+        ['Hansen Palomares', 51.4942, 0.1105, 8],
+        ['Morrison Spowart Solictors', 51.4451, 0.0195, 10],
+        ['TV Edwards Solicitors LLP', 51.5206, 0.0545, 14],
+        ['Wainwright & Cummins LLP', 51.4612, 0.1111, 16],
+        ['Bloomfield Solictors',51.49608546844893, 0.14225376265765458 , 16],
+        ['Hodge Jones & Allen Solicitors',51.526806003452805, -0.13696375268462269 , 16],
+        ['Sternberg Reed',51.544696915892246, 0.07990522620239444 , 16],
+        ['Grand Machyle Solicitors',51.39781704392408, -0.074954744044039, 16],
+        ['Brixton Advice Centre Legal Advice Clinic',51.45671594293158, -0.10590800171424246 , 16],
+        ['Disability Law Service',51.48593741258165, -0.11635653054935527, 16],
+        ['Edwards Duthie Shamash Solicitors',51.50301246302862, -0.11045168835330066, 16]
+
+
+
+    ];
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 11,
+        center: new google.maps.LatLng(51.4743, 0.0941),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
     });
 
-    const marker = new google.maps.Marker({
-        position: london,
-        map: map,
-    });
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            map: map
+        });
+
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infowindow.setContent(locations[i][0]);
+                infowindow.open(map, marker);
+            }
+        })(marker, i));
+    }
 }
--- Justenant_Application/script.js
+++ Justenant_Application/script.js
@@ -2,57 +2,151 @@
 function createPDF() {
 	var doc = new jsPDF();
 
-	doc.text("Tenant Report", 90, 10);
+	doc.text("Tenant Document", 90, 10);
 
 	var ai = document.getElementById("rent").value;
 
 	if (ai != "")
-		doc.text("Rent owed (£): " + ai, 20, 20);
+		doc.text("Rent Owed (£): " + ai, 20, 20);
 	else {
 		alert("ERROR..!!..Please Enter Rent. ");
+		return;
+	}
+
+	ai = document.getElementById("arrear").value;
+
+	if (ai != "")
+		doc.text("Arrears owed (£): " + document.getElementById("arrear").value, 20, 30);
+	else {
+		alert("ERROR..!!..Please enter Arrear Amount. ");
 		return;
 	}
 
 	ai = document.getElementById("current-residence").value;
 
 	if (ai != "")
-		doc.text("Time at Current Residence (weeks): " + document.getElementById("current-residence").value, 20, 30);
+		doc.text("Time at Current Residence (Years): " + document.getElementById("current-residence").value, 20, 40);
 	else {
 		alert("ERROR..!!..Please Enter Current Residence. ");
 		return;
 	}
-	ai = document.getElementById("occupant").value;
+
+
+	ai = document.getElementById("occupants").value;
+
 	if (ai != "")
-		doc.text("Occupants: " + ai, 20, 40);
+		doc.text("How many people live with you: " + ai, 20, 50);
 	else {
-		alert("ERROR..!!..Please Enter number of occupants. ");
+		alert("ERROR..!!..Please Enter Number of People Who Live With You. ");
 		return;
 	}
 
-	ai = document.getElementById("annual-income").value;
+
+
+	ai = document.getElementById("age").value;
+
 	if (ai != "")
-		doc.text("Annual Income (£): " + ai, 20, 50);
+		doc.text("Age of Occupant(s): " + ai, 20, 60);
 	else {
-		alert("ERROR..!!..Please Enter your Annual Income. ");
+		alert("ERROR..!!..Please Enter Age. ");
 		return;
 	}
 
 
-	if (document.getElementById("yes").checked || document.getElementById("no").checked)
-		document.getElementById("yes").checked == true ? doc.text("Significant mental/physical problems: Yes", 20, 60) : doc.text("Significant mental/physical problems: No", 20, 60);
+
+
+
+	if (document.getElementById("children").value != "")
+		doc.text("No. of Children: " + document.getElementById("children").value, 20, 70);
 	else {
-		alert("ERROR..!!..Please Check health problems. ");
+		alert("ERROR..!!..Please Enter No of Children. ");
 		return;
 	}
 
-	if (document.getElementById("yes1").checked || document.getElementById("no1").checked) {
-		document.getElementById("yes1").checked == true ? doc.text("Disrepair to home: Yes", 20, 70) : doc.text("Disrepair to home: No", 20, 70);
-	}
+
+	if (document.getElementById("relation").value != "")
+		doc.text("Relationship(s) to tenant: " + document.getElementById("relation").value, 20, 80);
 	else {
-		alert("ERROR..!!..Please Check Disrepair to Home. ");
+		alert("ERROR..!!..Please Enter Relation to Tenant. ");
 		return;
 	}
+
+	if (document.getElementById("annual-income").value != "")
+		doc.text("Monthly Income(£): " + document.getElementById("annual-income").value, 20, 100);
+	else {
+		alert("ERROR..!!..Please Enter Monthly Income(£). ");
+		return;
+	}
+
+
+
+
+	var y_coord = 90;
+	if (document.getElementById("benefits").checked) {
+		doc.text("Employment Status: Welfare Benefits", 20, 110);
+		doc.text("Detailed Welfare Benefits are:" + document.getElementById("detailedbenefits").value, 20, 120);
+		y_coord = 100;
+
+	}
+	else if (document.getElementById("self-employed").checked) {
+		doc.text("Employment Status: Self-Employed", 20, 110);
+		y_coord = 100;
+
+	}
+	else if (document.getElementById("employed").checked) {
+		doc.text("Employment Status: Employed", 20, 110);
+		y_coord = 100;
+	}
+	else {
+		alert("ERROR..!!..Please Choose Employment Status.");
+		return false;
+	}
+
+	if (document.getElementById("yes1").checked) {
+		doc.text("Any significant mental/physical health problems: Yes", 20, 130);
+		y_coord += 10;
+		doc.text("Detail of health problems are:" + document.getElementById("detailedproblems").value, 20, 140);
+		y_coord += 20;
+
+	}
+	else if (document.getElementById("no1").checked) {
+		doc.text("Any significant mental/physical health problems: No", 20, 130);
+		y_coord += 10;
+
+	}
+	else {
+		alert("ERROR..!!..Please Choose Mental/Physical Problem status.");
+		return false;
+	}
+
+	if (document.getElementById("yes2").checked) {
+		doc.text("Any disrepair to your home: Yes", 20, 150);
+		y_coord += 10;
+		doc.text("Detail of disrepair are:" + document.getElementById("detaileddisrepair").value, 20, 160);
+		y_coord += 20;
+
+	}
+	else if (document.getElementById("no2").checked) {
+		doc.text("Any disrepair to your home: No", 20, 150);
+		y_coord += 10;
+
+	}
+	else {
+		alert("ERROR..!!..Please Choose disrepair to your home.");
+		return false;
+	}
+
 
 	doc.save("tenant-report.pdf");
-	alert("Your form has been submitted, Please explore the document in pdf format. ");
+	alert("Your form is submitted, Please explore the document in pdf format. ");
+
 }
+
+function hidedivdbenefits() {
+	document.getElementById('divdbenefits').style.display = 'none'
+}
+function showdivdbenefits() { document.getElementById('divdbenefits').style.display = 'block' }
+function showdivproblems() { document.getElementById('divproblems').style.display = 'block' }
+function hidedivproblems() { document.getElementById('divproblems').style.display = 'none' }
+function showdisrepair() { document.getElementById('divdisrepair').style.display = 'block' }
+function hidedisrepair() { document.getElementById('divdisrepair').style.display = 'none' }
